1f1120bef19f31f913da90800f0211107aaf0ae2
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-09-10 10:20:28 -0500

    genre search menu items

--- src/components/GenreSearch/index.js
+++ src/components/GenreSearch/index.js
@@ -7,12 +7,30 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 
+
 const GenreSearch = () => {
   
     return (
       <div>Genre Search
-        <input type='list'></input>
-        <button>Search by Genre</button>
+        <FormControl sx={{ m: 1, minWidth: 160}} size='small'>
+          <InputLabel>Genre</InputLabel>
+            <Select>
+              <MenuItem value='1'>Action</MenuItem>
+              <MenuItem value='4'>Comedy</MenuItem>
+              <MenuItem value='6'>Documentary</MenuItem>
+              <MenuItem value='7'>Drama</MenuItem>
+              <MenuItem value='40'>Fantasy</MenuItem>
+              <MenuItem value='10'>History</MenuItem>
+              <MenuItem value='11'>Horror</MenuItem>
+              <MenuItem value='32'>Musical</MenuItem>
+              <MenuItem value='13'>Mystery</MenuItem>
+              <MenuItem value='14'>Romance</MenuItem>
+              <MenuItem value='15'>Science Fiction</MenuItem>
+              <MenuItem value='17'>Thriller</MenuItem>
+              <MenuItem value='18'>War</MenuItem>
+            </Select>
+            <FormHelperText>Select a Genre</FormHelperText>
+        </FormControl>
 
       </div>
     );
